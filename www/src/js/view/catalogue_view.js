71f11a03d87a1eec1730ddde1840d97420bd34c4
Raúl Yeguas <raul.yeguas@geographica.gs>
2014-05-19 18:27:48 +0200

    Continuación de la maquetación de catágolo. Subvistas.

--- www/src/js/view/catalogue_view.js
+++ www/src/js/view/catalogue_view.js
@@ -2,7 +2,10 @@ app.view.Catalogue = Backbone.View.extend({
 	_template : _.template( $('#catalogue_template').html() ),
 
 	initialize: function() {
-        this.render();
+        this.collection = new app.collection.Categories();
+        this.listenTo(this.collection, 'reset', this.render );
+        this.collection.fetch({reset: true});
+        //this.render();
     },
     
     onClose: function(){
