a3e3849be6402ef4ed020a5b997a64aa4980451a
Raúl Yeguas <raul.yeguas@geographica.gs>
2014-05-20 08:38:05 +0200

    Eliminada la carpeta node_modules

--- www/src/js/collection/categories.js
+++ www/src/js/collection/categories.js
@@ -1,4 +1,8 @@
 app.collection.Categories = Backbone.Collection.extend({
 	model: app.model.Category,
-	url: '/js/catalog.js'
+	url: '/js/catalog.js',
+
+	fetch: function(){
+		console.log('here');
+	}
 });
