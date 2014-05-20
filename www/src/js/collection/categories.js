50326f60fe552f2aa1a88d15a43c5ddc776879bc
Raúl Yeguas <raul.yeguas@geographica.gs>
2014-05-20 10:44:49 +0200

    Eliminación del venv (intento 2)

--- www/src/js/collection/categories.js
+++ www/src/js/collection/categories.js
@@ -1,11 +1,3 @@
 app.collection.Categories = Backbone.Collection.extend({
-	model: app.model.Category,
-	url: '/js/catalog.js',
-	parse: function (data) {
-        if (_.isObject(data.results)) {
-            return data.results;
-        } else {
-            return data;
-        }
-    }
+	model: app.model.Category
 });
