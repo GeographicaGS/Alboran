2ddc1cb1b7b4d516f3035c4396faefde401a5bba
Raúl Yeguas <raul.yeguas@geographica.gs>
2014-05-21 10:13:46 +0200

    Maquetación de Catálogo

--- www/src/js/view/layer_view.js
+++ www/src/js/view/layer_view.js
@@ -3,7 +3,8 @@ app.view.Layer = Backbone.View.extend({
 	className: 'layerItem',
 
 	events: {
-		'click .info_btn': 'toggle'
+		'click .info_btn': 'toggle',
+		'click .add_btn': 'toggleLayer'
 	},
 
 	initialize: function() {
@@ -19,13 +20,26 @@ app.view.Layer = Backbone.View.extend({
 		this.$el.html(this._template( this.model ));
 
 		this.$info = this.$('.info');
-
+		this.$addBtn = this.$('.add_btn');
 
         return this;
 	},
 
-	toggle: function(e){
+	toggle: function(e) {
 		e.preventDefault();
 		this.$info.toggleClass('show');
+	},
+
+	toggleLayer: function(e) {
+		e.preventDefault();
+		if(this.$addBtn.hasClass('add')){
+			var that = this;
+			this.$addBtn.removeClass('add');
+			setTimeout(function() {that.$addBtn.html(that.$addBtn.attr('removelabel'));},300);
+			
+		}else{
+			this.$addBtn.addClass('add');
+			this.$addBtn.html(this.$addBtn.attr('addlabel'));
+		}
 	}
 });
