ff3b1ac8ba0469af2891dcd3492df62772da8790
Alberto Asuero <alberto.asuero@geographica.gs>
2014-06-11 08:46:28 +0200

    Changes of confi_changes.py

--- api/api/config_changes.py
+++ api/api/config_changes.py
@@ -3,23 +3,26 @@ Alboran frontend configuration.
 """
 from api import app
 
+baseRootPath = ""
+cdnPath = baseRootPath + "/www/cdn"
+
 app.config.update(
     DEBUG=False,
-    SECRET_KEY='',
-    smtpServer='',
-    smtpPort=,
-    smtpUser='',
-    smtpPassword='',
-    smtpTLS=False,
-    smtpAuth=False,
-    smtpFromAddr='',
-    smtpFromAddrName='',
-    UPLOAD_TEMP_FOLDER='../tempimages/',
-    IMAGES_FOLDER='../images/',
-    ALLOWED_EXTENSIONS=set(['jpg','jpeg','png']),
+    SECRET_KEY="XXXX",
+    baseURL = "",
+    smtpServer="localhost",
+    smtpPort=25,
+    smtpUser="",
+    smtpPassword="",
+    smtpFromAddr="",
+    smtpFromAddrName="DEV alboran",
+    smtpTLS = False,
+    smtpAuth = False,
+    convertBin = "convert",
+    UPLOAD_TEMP_FOLDER= baseRootPath +"/tempimages/",
+    IMAGES_FOLDER= cdnPath  +"/images/",
+    ALLOWED_EXTENSIONS=set(["jpg","jpeg","png"]),
     MAX_CONTENT_LENGTH=16 * 1024 * 1024,
-    convertBin='',
-    baseURL='',
     historyFirstPagSize=,
     historyPagSize=
 )
