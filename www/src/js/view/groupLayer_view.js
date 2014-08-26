95d79dc7a1eab94f018d5f1027c9ece0cda427c6
Javier Aragón <javier.aragon@geographica.gs>
2014-08-26 18:54:43 +0200

    bugs

--- www/src/js/view/groupLayer_view.js
+++ www/src/js/view/groupLayer_view.js
@@ -261,7 +261,8 @@ app.view.GroupLayer = Backbone.View.extend({
             break;
         }
         
-        $li.html("<p class='ellipsis fleft' title='" +  layer["title_" + app.lang] + "'>" + layer["title_" + app.lang] + "</p>");
+        // $li.html("<p class='ellipsis fleft' title='" +  layer["title_" + app.lang] + "'>" + layer["title_" + app.lang] + "</p>");
+        $li.html("<p class='ellipsis fleft' title='" +  layer["title_es"] + "'>" + layer["title_es"] + "</p>");
         
         var $icon1, $icon2, $icon3, $icon4;
         
