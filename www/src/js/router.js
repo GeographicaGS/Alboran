b9584eea3f9a187a191737da9f69b45de392996b
Javier Aragón <javier.aragon@geographica.gs>
2014-11-05 16:38:21 +0100

    ajustes

--- www/src/js/router.js
+++ www/src/js/router.js
@@ -13,7 +13,7 @@ app.router = Backbone.Router.extend({
         "_link join" : {"en":"participate","es": "participe", "fr": "participer" },
         "_link writehistory" : {"en":"writehistory","es": "escribirhistoria", "fr": "escribirhistoria" },
         "_link history" : {"en":"history","es": "historia", "fr": "histoire" },
-        "_link legal" : {"en":"legal","es": "legal", "fr": "legal" },
+        "_link legal" : {"en":"legal","es": "legal", "fr": "juridique" },
         "_link privacy" : {"en":"privacy","es": "privacidad", "fr": "privacidad" },
         "_link user" : {"en":"user","es": "usuario", "fr": "usuario" }
     },
